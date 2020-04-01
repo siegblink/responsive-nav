0dfa5fcca503524df98eed2b5d8f179581bd9fd7
Sieg <siegblink@gmail.com>
2020-04-01 11:07:14 +0800

    Load initial project setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router } from 'reach-router'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import './App.css'
 
 function App() {
+  const navLinks = [
+    { text: 'Home', path: '/home', icon: 'ion-ios-home' },
+    { text: 'Contact', path: '/contact', icon: 'ion-ios-megaphone' },
+    { text: 'About', path: '/about', icon: 'ion-ios-business' },
+    { text: 'Blog', path: '/blog', icon: 'ion-ios-bonfire' },
+    { text: 'Portfolio', path: '/portfolio', icon: 'ion-ios-briefcase' },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Contact path='/contact' />
+        <Home path='/home' />
+        <Portfolio path='/portfolio' />
+        <Blog path='/blog' />
+        <About path='/about' />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
